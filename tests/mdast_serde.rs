ed46bc3574bc36573296d025b8a1309dfb74b8f8
Dominik Stumpf <s.dominik.work@gmail.com>
2024-02-08 10:13:16 +0100

    test: add basic deserialization and serialization

---
+++ tests/mdast_serde.rs
@@ -0,0 +1,24 @@
+use markdown::mdast::Node;
+use serde_json;
+
+#[test]
+fn serde_json_serialization() -> Result<(), String> {
+    let markdown = "> Alpha bravo charlie";
+    let mdast_json_str = r#"{"type":"root","children":[{"type":"blockquote","children":[{"type":"paragraph","children":[{"type":"text","value":"Alpha bravo charlie","position":{"start":{"line":1,"column":3,"offset":2},"end":{"line":1,"column":22,"offset":21}}}],"position":{"start":{"line":1,"column":3,"offset":2},"end":{"line":1,"column":22,"offset":21}}}],"position":{"start":{"line":1,"column":1,"offset":0},"end":{"line":1,"column":22,"offset":21}}}],"position":{"start":{"line":1,"column":1,"offset":0},"end":{"line":1,"column":22,"offset":21}}}"#;
+    let mdast = markdown::to_mdast(&markdown, &markdown::ParseOptions::default()).unwrap();
+    let mdast_json = serde_json::to_string(&mdast).unwrap();
+    assert_eq!(mdast_json, mdast_json_str);
+
+    Ok(())
+}
+
+#[test]
+fn serde_json_deserialization() -> Result<(), String> {
+    let markdown = "> Alpha bravo charlie";
+    let mdast_json_str = r#"{"type":"root","children":[{"type":"blockquote","children":[{"type":"paragraph","children":[{"type":"text","value":"Alpha bravo charlie","position":{"start":{"line":1,"column":3,"offset":2},"end":{"line":1,"column":22,"offset":21}}}],"position":{"start":{"line":1,"column":3,"offset":2},"end":{"line":1,"column":22,"offset":21}}}],"position":{"start":{"line":1,"column":1,"offset":0},"end":{"line":1,"column":22,"offset":21}}}],"position":{"start":{"line":1,"column":1,"offset":0},"end":{"line":1,"column":22,"offset":21}}}"#;
+    let mdast_deserialized = serde_json::from_str::<Node>(mdast_json_str).unwrap();
+    let mdast = markdown::to_mdast(&markdown, &markdown::ParseOptions::default()).unwrap();
+    assert_eq!(mdast, mdast_deserialized);
+
+    Ok(())
+}
